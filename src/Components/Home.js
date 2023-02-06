00070ae59c86ab2ad5d2afe320fe79f26ded3cb5
Dhruv Aeron <dhruv.aeron3@gmail.com>
2023-02-06 18:46:08 +0530

    where section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,10 +5,10 @@ import { MdOutlineComputer } from "react-icons/md";
 function Home() {
   return (
     <>
-      <div>
+      <div className="">
         {/* NAVBAR */}
 
-        <section className=" flex gap-x-96 flex-row border-b-2 border-slate-700 h-16 justify-center items-center sticky">
+        <section className=" flex gap-x-96 flex-row border-b-2 border-slate-700 h-16 justify-center items-center ">
           <div
             className="text-xl text-white
           "
@@ -36,7 +36,7 @@ function Home() {
 
         {/* HERO */}
 
-        <section className="container mx-auto ">
+        <section className="container mx-auto h-[85vh]">
           <div className="flex flex-row ">
             {/* LEFT HERO */}
             <div className="basis-1/2 pt-32 pl-16">
@@ -61,6 +61,50 @@ function Home() {
             <div className="basis-1/2 flex justify-center items-center text-lred  ">
               TO BE CHANGED
               <MdOutlineComputer color="#C4001D" size={200} />
+            </div>
+          </div>
+        </section>
+
+        <div className="relative">
+          <div className="arrow"></div>
+        </div>
+
+        {/* WHEN && WHERE */}
+
+        <section className="container mx-auto mt-[15vh]">
+          <div className="flex flex-row gap-x-80 ml-16">
+            {/* LEFT */}
+
+            <div className="basis-1/3 ">
+              <img
+                src="https://snazzy-maps-cdn.azureedge.net/assets/1261-dark.png?v=20170626074646"
+                alt="map"
+                className="rounded"
+              />
+            </div>
+
+            {/* RIGHT  */}
+
+            <div className="basis-2/3 flex flex-row gap-24">
+              <div className="flex justify-center items-center flex-col ">
+                <div>
+                  {" "}
+                  <span className="text-mred font-medium">When?</span> <br />
+                  <span className="text-slate-300 text-3xl font-bold">
+                    1st-6th <br /> of March <br /> 2023
+                  </span>
+                </div>
+              </div>
+              <div className="flex justify-center items-center flex-col">
+                <div>
+                  <span className="text-mred font-medium">Where?</span> <br />
+                  <span className="text-slate-300 text-3xl font-bold">
+                    USICT, GGSIPU <br />
+                    Sector - 16 <br />
+                    New Delhi
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
         </section>
