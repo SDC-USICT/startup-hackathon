bd696e106eed72216ba7f6b20faead1ea3ae9a1e
Dhruv Aeron <dhruv.aeron3@gmail.com>
2023-02-06 17:58:18 +0530

    hero section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./CSS/home.css";
+import { MdOutlineComputer } from "react-icons/md";
 function Home() {
   return (
     <>
       <div>
-        {/* navbar */}
+        {/* NAVBAR */}
+
         <section className=" flex gap-x-96 flex-row border-b-2 border-slate-700 h-16 justify-center items-center sticky">
-                  <div className="text-xl text-white
-          ">Logo</div>
+          <div
+            className="text-xl text-white
+          "
+          >
+            Logo
+          </div>
           <div className="flex flex-row gap-x-16 justify-center items-center">
             <div className="text-slate-200  hover:text-slate-400 cursor-pointer hover:border-b-2 border-red-900 px-1 hover:ease-out duration-100 text-l">
               About
@@ -22,12 +28,42 @@ function Home() {
             <div className="text-slate-200  hover:text-slate-400 cursor-pointer hover:border-b-2 border-red-900 px-1 hover:ease-out duration-100">
               Contact Us
             </div>
-            <div className="bg-red-600 hover:bg-red-700 px-8 py-2 rounded-lg text-slate-200 hover:text-slate-300 hover:bg-red-650 cursor-pointer">
-              <div className="">Register</div>
+            <div className="bg-lred hover:bg-mred px-8 py-2 rounded-lg text-slate-200 hover:text-slate-300 hover:bg-red-650 cursor-pointer font-semibold">
+              <div className="">REGISTER</div>
             </div>
           </div>
         </section>
-        {/* hero */}
+
+        {/* HERO */}
+
+        <section className="container mx-auto ">
+          <div className="flex flex-row ">
+            {/* LEFT HERO */}
+            <div className="basis-1/2 pt-32 pl-16">
+              <div className="text-7xl font-semibold text-slate-200">
+                Startup hackathon
+              </div>
+              <div className="text-mred text-xl mt-2">
+                learn | teach | explore
+              </div>
+              <div className="text-justify text-gray-400 mt-2">
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, recusandae natus tenetur sed voluptate quas aliquam
+                itaque debitis, vel quae doloribus nostrum facilis nemo
+                voluptatibus, quaerat animi nulla aperiam excepturi.{" "}
+              </div>
+              <div className="bg-lred hover:bg-mred px-8 py-2 rounded-lg text-slate-200 hover:text-slate-300 hover:bg-red-650 cursor-pointer  w-fit font-semibold mt-24">
+                REGISTER
+              </div>
+            </div>
+            {/* RIGHT HERO  */}
+            <div className="basis-1/2 flex justify-center items-center text-lred  ">
+              TO BE CHANGED
+              <MdOutlineComputer color="#C4001D" size={200} />
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lred: "#E5193C",
+        mred: "#C4001D",
+      },
+    },
   },
   plugins: [],
 };
